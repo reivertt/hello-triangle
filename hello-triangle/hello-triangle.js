c2fc89fc3a3b104c3d82ef5feedb2e31eec47843
reivertt <billthehuman27@gmail.com>
2025-09-02 14:33:19 +0700

    init

---
+++ hello-triangle/hello-triangle.js
@@ -0,0 +1,9 @@
+function showError(errorText) {
+    const errorBoxDiv = document.getElementById("error-box");
+    const errorTextElement = document.createElement("p");
+    errorTextElement.innerText = errorText;
+    errorBoxDiv.appendChild(errorTextElement);
+    console.log(errorText);
+}
+
+showError("This is a test error message.");
